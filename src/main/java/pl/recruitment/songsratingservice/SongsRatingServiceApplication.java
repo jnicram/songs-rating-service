9573257b0964ee01db8af787ddd1a380eaeac49c
Marcin Jasinski <jnicram@gmail.com>
2024-01-09 12:47:28 +0100

    Initial structure setup

---
+++ src/main/java/pl/recruitment/songsratingservice/SongsRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.recruitment.songsratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongsRatingServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SongsRatingServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/pl/recruitment/songsratingservice/SongsRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.recruitment.songsratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongsRatingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
